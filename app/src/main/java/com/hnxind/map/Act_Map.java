e4547ec8191bee6ac1fc063fc7a711a23fc7a86c
mangocandy <1638005026@qq.com>
2016-01-15 17:05:30 +0800

    up

--- app/src/main/java/com/hnxind/map/Act_Map.java
+++ app/src/main/java/com/hnxind/map/Act_Map.java
@@ -1,10 +1,7 @@
 package com.hnxind.map;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
